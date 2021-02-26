5d02e361ab3336f4c3dfd79147f6052c09920595
jacobrg22 <77154810+jacobrg22@users.noreply.github.com>
2021-02-25 21:38:14 -1000

    Update 2/25/21

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -4,8 +4,9 @@ num_products = 5
 
 item_num = 1
 
-while (item_num != (num_products +1)) {
-    console.log(item_num);
+while (item_num <= (num_products/2)) {
+    console.log(`${item_num}. ${eval('name' + item_num)}`);
     item_num++;
-    }
+}
     
+console.log("That's all we have!");
---
+++ Lab8/Ex2.js
@@ -0,0 +1,15 @@
+require("./products_data.js");
+
+num_products = 5
+
+item_num = 0
+
+while (item_num++ < (num_products)) {
+    if(item_num >= 0.25*num_products && item_num <= 0.75*num_products) {
+    console.log(`Don't ask for anything else!`)
+    process.exit();
+    }   
+    console.log(`${item_num}. ${eval('name' + item_num)}`);
+}
+    
+console.log("That's all we have!");
