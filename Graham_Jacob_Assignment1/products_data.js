b3535ea6f5e120ee06bdc0be2eccd34e38fccfae
jacobrg22 <77154810+jacobrg22@users.noreply.github.com>
2021-03-29 16:27:48 -1000

    Update 3/29/21

--- Graham_Jacob_Assignment1/products_data.js
+++ Graham_Jacob_Assignment1/products_data.js
@@ -1,46 +1,39 @@
 // products object
 // Descriptions adapted from https://bulbapedia.bulbagarden.net/wiki/ and https://www.pokemon.com/us/pokemon-tcg/product-gallery/
 var products = [ 
-    {   
-        "brand" : "Torterra LV.X",
-        "image" : "./images/torterra.jpg",
-        "price" : 4.95,
-        "desc" : "Torterra is a Grass-type Level-Up Pokémon LV.X card. It was first released as part of the Diamond & Pearl expansion. A purchase will net you 1x Toterra LV.X card.",
-        "quantity" : 0
-    },
     {
         "brand" : "Arceus LV.X",
         "image" : "./images/arceus.jpg",
         "price" : 4.95,
         "desc" : "Arceus is a Colorless-type Level-Up Pokémon LV.X card. It was first released as part of the Arceus expansion.",
-        "quantity" : 0
+        "quantity" : 5
+    },
+    {   
+        "brand" : "Torterra LV.X",
+        "image" : "./images/torterra.jpg",
+        "price" : 4.95,
+        "desc" : "Torterra is a Grass-type Level-Up Pokémon LV.X card. It was first released as part of the Diamond & Pearl expansion.",
+        "quantity" : 5
     },
     {
         "brand" : "Shining Fates Crobat Premium Collection",
         "image" : "./images/crobat_coll.jpg",
         "price" : 39.95,
         "desc" : "Comes with 7 booster packs, 1x Dragapult V, 1x Dragapult VMAX, a promotional jumbo coin, and one promotional jumbo card.",
-        "quantity" : 0
+        "quantity" : 10
     },
     {
         "brand" : "Shining Fates Dragapult Premium Collection",
         "image" : "./images/dragapult_coll.jpg",
         "price" : 39.95,
         "desc" : "Comes with 7 booster packs, 1x Dragapult V, 1x Dragapult VMAX, a promotional jumbo coin, and one promotional jumbo card.",
-        "quantity" : 0
-    },
-    {
-        "brand" : "Sky Legends Booster Box Japanese",
-        "image" : "./images/sky_legends_box_j.jpg",
-        "price" : 49.95,
-        "desc" : "Contains 30 booster packs of the Japan-only Sky Legends expansion to the Pokemon TCG. All cards are in Japanese.",
-        "quantity" : 0
+        "quantity" : 10
     },
     {
         "brand" : "Champion's Path Elite Trainer Box",
         "image" : "./images/charizard_etb.jpg",
         "price" : 49.95,
         "desc" : "Comes with 10 booster packs, 1x Charizard V, 65 card sleeves, 45 Pokémon TCG Energy cards, and a box that you can store pokemon cards in.",
-        "quantity" : 0
+        "quantity" : 5
     }
 ]
--- Graham_Jacob_Assignment1/server.js
+++ Graham_Jacob_Assignment1/server.js
@@ -1,1 +1,7 @@
 // Setup server
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('.'));
+app.listen(8080, () => console.log(`listening on port 8080`));
