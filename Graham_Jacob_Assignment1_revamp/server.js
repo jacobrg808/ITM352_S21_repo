07a538bd6c901b39efbcf7fe783758751144b6d9
jacobrg22 <77154810+jacobrg22@users.noreply.github.com>
2021-04-07 19:09:35 -1000

    Update 4/6/21 2

--- Graham_Jacob_Assignment1_revamp/server.js
+++ Graham_Jacob_Assignment1_revamp/server.js
@@ -2,6 +2,19 @@
 
 var express = require('express');
 var app = express();
+// var myParser = require("body-parser")
+// app.use(myParser.urlencoded({ extended: true }));
 
-app.use(express.static('.'));
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path + 'with query' + JSON.stringify(request.query));
+    next();
+});
+
+app.post('./public/products_display.html', function (request, response, next) {
+    post_data = request.body;
+    response.send(post_data);
+});
+
+app.use(express.static('./public'));
+
 app.listen(8080, () => console.log(`listening on port 8080`));
