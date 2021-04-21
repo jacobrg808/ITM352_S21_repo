be3bf836fbc4691e3fa436d232ac4a6f78ceb96a
jacobrg22 <77154810+jacobrg22@users.noreply.github.com>
2021-04-20 14:20:59 -1000

    Update 4/20/21 3

--- Graham_Jacob_Assignment2/server.js
+++ Graham_Jacob_Assignment2/server.js
@@ -17,10 +17,10 @@ app.all('*', function (req, res, next) {
 
 app.use(myParser.urlencoded({ extended: true }));
 
-// set userData equal to user_data.json
+// Set userData equal to user_data.json
 var userData = './user_data.json'; 
 
-// code from Lab14, modified for Assignment2
+// Code from Lab14, modified for Assignment2
 if(fs.existsSync(userData)) { 
     var file_stats = fs.statSync(userData);
     console.log(`${userData} has ${file_stats.size} characters.`);
@@ -32,14 +32,14 @@ else {
 }
 
 // Thanks to NOAH KIM for giving guidance on the functions and statements
-// structure of process_login function from Lab14, modified for Assignment2
+// Structure of process_login function from Lab14, modified for Assignment2
 app.post('/process_login', function (req, res) {
     var LogError = [];
     console.log(req.query);
     username = req.body.username.toLowerCase();
-    // check to see if info is defined
+    // Check to see if info is defined
     if (typeof users_reg_data[username] != 'undefined') {
-        // if info is good then redirect to invoice
+        // If info is good then redirect to invoice
         if (users_reg_data[username].password == req.body.password) {
             req.query.username = username;
             console.log(users_reg_data[req.query.username].name);
@@ -47,44 +47,44 @@ app.post('/process_login', function (req, res) {
             res.redirect('./invoice.html?' + queryString.stringify(req.query));
             return;
         }
-        // else push an error
+        // Else push an error
         else {
-            LogError.push = ('Sorry, but the password you inputted is invalid!');  // inform user that they inputted a wrong username
+            LogError.push = ('Sorry, but the password you inputted is invalid!');  // Inform user that they inputted a wrong username
             console.log(LogError);
             req.query.username = username;
             req.query.name = users_reg_data[username].name;
             req.query.LogError = LogError.join(';');
         }
     }
-    // else push an error
+    // Else push an error
     else {
-        LogError.push = ('Sorry, but the username you inputted is invalid!'); // inform user that they inputted a wrong username
+        LogError.push = ('Sorry, but the username you inputted is invalid!'); // Inform user that they inputted a wrong username
         console.log(LogError);
         req.query.username = username;
         req.query.LogError = LogError.join(';');
     }
-    res.redirect('./login.html?' + queryString.stringify(req.query)); // redirect to login page
+    res.redirect('./login.html?' + queryString.stringify(req.query)); // Redirect to login page
 });
 
 // Thanks to NOAH KIM for giving guidance on the functions and statements
-// structure of process_register function from Lab14, modified for Assignment2
+// Structure of process_register function from Lab14, modified for Assignment2
 app.post("/process_register", function (req, res) {
     qstr = req.body
     console.log(qstr);
     var errors = [];
     // Setup character limitations (Letters only for name)
-    if (/^[A-Za-z]+$/.test(req.body.name)) {
+    if (/^[a-zA-Z]+[ ]+[a-zA-Z]+$/.test(req.body.name)) {
     }
     else {
-        errors.push('Please only use letters for your name!')
+        errors.push('Please follow the format for names! (ex. John Smith)')
     }
     // If the entered name is empty, return an error
     if (req.body.name == "") {
         errors.push('The full name inputted is invalid.');
     }
     // Set a maximum fullname length of 30
-    if ((req.body.fullname.length > 30 && req.body.fullname.length < 0)){ // Output errors if the password is too long
-        errors.push('Sorry! That password is too long.')
+    if ((req.body.fullname.length > 30 && req.body.fullname.length < 0)){ // Output errors if the name is too long
+        errors.push('Sorry! That name is too long.')
     }
     // Check to see that the newly registered username is lower case
     var reguser = req.body.username.toLowerCase(); 
@@ -98,6 +98,13 @@ app.post("/process_register", function (req, res) {
         errors.push('Please use only letters and numbers for your username.')
     }
   
+    // Setup email limitations (from w3resource https://www.w3resource.com/javascript/form/email-validation.php)
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(req.body.email)) {
+    }
+    else {
+        errors.push('Please use a valid format email format (ex. johnsmith@gmail.com)')
+    }
+
     // Make password a minimum of six characters
     if (req.body.password.length < 6) {
         errors.push('Your password is too short (Please use at least 6 characters).')
@@ -107,12 +114,12 @@ app.post("/process_register", function (req, res) {
         errors.push('Your password does not match.')
     }
 
-    // request fullname, username, and email
+    // Request fullname, username, and email
     req.query.fullname = req.body.fullname;
     req.query.username = req.body.username;
     req.query.email = req.body.email;
 
-    // Rememeber user information given no errors
+    // Remember user information given no errors
     if (errors.length == 0) {
         POST = req.body;
         console.log('no errors');
@@ -121,36 +128,36 @@ app.post("/process_register", function (req, res) {
         users_reg_data[username].name =  req.body.fullname;
         users_reg_data[username].password =  req.body.password;
         users_reg_data[username].email =  req.body.email;
-        data = JSON.stringify(users_reg_data); // stringify user's info
+        data = JSON.stringify(users_reg_data); // Stringify user's info
         fs.writeFileSync(userData, data, "utf-8");
         res.redirect('./invoice.html?' + queryString.stringify(req.query));
     }
-    // check for errors
+    // Check for errors
     else {
         console.log(errors)
         req.query.errors = errors.join(';');
-        res.redirect('register.html?' + queryString.stringify(req.query)); // redirect to register.html
+        res.redirect('register.html?' + queryString.stringify(req.query)); // Redirect to register.html
     }
 });
 
 // Derived from Lab 14 and Assigment2 screencast
-// take form and process it given that the information submitted is ok
+// Take form and process it given that the information submitted is ok
 app.post("/process_purchase", function (req, res) {
     let POST = req.body;
     console.log(POST)
     if (typeof POST['purchase_submit'] != 'undefined') {
-        var hasValidQty = true; // assume that quantity is valid
+        var hasValidQty = true; // Assume that quantity is valid
         var hasQty = false;
         for (i = 0; i < products.length; i++) {
-            quantity = POST[`quantity${i}`]; // set QtyCheck equal to POST[`quantity${i}`]
+            quantity = POST[`quantity${i}`]; // Set QtyCheck equal to POST[`quantity${i}`]
             if (quantity > 0) {
-                hasQty = true // has non-zero quantity
+                hasQty = true // Has non-zero quantity
             }
             if (isNonNegInt(quantity) == false) {
-                hasValidQty = false // has both valid quantity and non-negative integer
+                hasValidQty = false // Has both valid quantity and non-negative integer
             }
         }
-        // given the the quantities inputted are valid, redirect the user back to index
+        // Given the the quantities inputted are valid, redirect the user back to index
         const stringified = queryString.stringify(POST); // If all quantities are valid then go to login.html with query string containing the order quantities
         if (hasValidQty && hasQty) {
             res.redirect("./login.html?" + stringified); // Directs user from products_display.html to login.html with the query string that has the order quantities
@@ -164,8 +171,8 @@ app.post("/process_purchase", function (req, res) {
 // isNonNegInt function from Lab11
 // Check to see if the quantity inputted is valid
 function isNonNegInt(q, return_errors = false) {
-    var errors = []; // assume no errors at first
-    if(q == '') q = 0; // if text box is blank, show nothing
+    var errors = []; // Assume no errors at first
+    if(q == '') q = 0; // If text box is blank, show nothing
     if (Number(q) != q) errors.push('<font color="red">Please input a number </font>'); // Check if string is a number value
     else {
         if (q < 0) errors.push('<font color="red">Please input a positive quantity </font>'); // Check if it is non-negative
@@ -174,9 +181,9 @@ function isNonNegInt(q, return_errors = false) {
     return return_errors ? errors : (errors.length == 0);
 }
 
-app.use(express.static('./public')); // use express.static
+app.use(express.static('./public')); // Use express.static
 
 app.listen(8080, () => console.log(`
     listening on port 8080
-    access here: http://localhost:8080/
-`)); // output to console the port we are listening in on
+    access here: http://itm-vm.shidler.hawaii.edu:8080/
+`)); // Output to console the port we are listening in on
